6f0317a24f8c8e72d197db01a68a6b114f8dc33a
AllanMwirigi <allo.mwirigi@gmail.com>
2022-12-13 04:54:22 +0300

    feat: configure testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.ts',
+  },
+};
